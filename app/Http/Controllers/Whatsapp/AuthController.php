db63a2e61b8e37e47ef2221ada9636a2b591a21f
Mupati <scientificgh@gmail.com>
2021-01-07 14:31:05 +0000

    Add whatsapp-clone api endpoints

---
+++ app/Http/Controllers/Whatsapp/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Whatsapp;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+
+    private function generateToken($request)
+    {
+        $token = $request->user()->createToken('wossop_token');
+        return ['token' => $token->plainTextToken];
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            Log::info('type: Login, user:' . $request->email . ', datetime:' . now()->toDateTimeString());
+
+            $token = $this->generateToken($request)['token'];
+            return response()->json(['user' => Auth::user(), 'token' => $token], 200);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.']
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed']
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        Log::info('type: registration, user:' . $request->email . ', status: account_created');
+
+        return response()->json(['success' => true, 'message' => 'Your account has been successfully created'], 201);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        Log::info('type: Logout, user:' . Auth::user()->email . ', datetime:' . now()->toDateTimeString());
+
+        return response()->json(['success' => true, "message" => "Logout successfull"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', 'App\Http\Controllers\Whatsapp\AuthController@logout');
 });
+Route::post('/register', 'App\Http\Controllers\Whatsapp\AuthController@register');
+Route::post('/login', 'App\Http\Controllers\Whatsapp\AuthController@login');
