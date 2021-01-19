728e6543b945ba7f08b0a319301922405f3d7f91
Mupati <scientificgh@gmail.com>
2021-01-19 12:53:41 +0000

    Add SEO tags

--- resources/views/agora-chat.blade.php
+++ resources/views/agora-chat.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('scripts')
+    <meta name="description" content="Build A Scalable Video Chat Application With Agora" />
+    <meta name="keywords" content="Video Call, Agora, Laravel, Real Time Engagement" />
+    <meta name="author" content="Kofi Obrasi Ocran" />
     <script src="https://cdn.agora.io/sdk/release/AgoraRTCSDK-3.3.1.js"></script>
 @endsection
 
